c8e64af6de805506d0a9d6514cb5cf0418963158
aashir12 <aashirnawaz5@gmail.com>
2026-07-28 23:43:04 +0500

    added utils & helper

---
+++ src/utils/helpers.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class helpers {
+     public static Integer addTwoVairiable(int a , int b){
+        return a+b;
+    }
+    public static Integer addThreeVariables(int a,int b, int c){
+         return a+b+c;
+    }
+
+}
