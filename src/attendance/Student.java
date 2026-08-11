390072b1cd8258a26f063c68ea77f4dc4a7cf12f
aashir12 <aashirnawaz5@gmail.com>
2026-08-12 02:48:24 +0500

    local class

--- src/attendance/Student.java
+++ src/attendance/Student.java
@@ -1,7 +1,9 @@
 package attendance;
 
 public class Student {
+
     public static int rollNumber = 0;
+
     private String name;
     private String className;
 
@@ -21,11 +23,28 @@ public class Student {
         this.className = className;
     }
 
-    static{
+    // This runs only once when the Student class is loaded
+    static {
         rollNumber++;
     }
 
-    Student(){
-        System.out.println("Student No = "+rollNumber);
+    Student() {
+        System.out.println("Student No = " + rollNumber);
+    }
+
+    class Uniform {
+        boolean tie;
+        boolean shoePolished;
+        boolean officialShoe;
+        boolean hairCut;
+    }
+
+    void setUniform() {
+        Uniform student1 = new Uniform();
+
+        student1.tie = true;
+        student1.shoePolished = true;
+        student1.officialShoe = true;
+        student1.hairCut = true;
     }
 }
