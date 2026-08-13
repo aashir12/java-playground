e070e8f9f6b4bac98d2293edbba44999b8fa81d2
aashir12 <aashirnawaz5@gmail.com>
2026-08-14 02:24:56 +0500

    compared two inner classes

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,20 @@ import test.Car;
 public class Main {
     public static void main(String[] args) {
         Student aashir = new Student();
+        Student shayan = new Student();
+        shayan.setName("Shayan");
+        shayan.setClassName("1st year");
         aashir.setName("Aashir");
         aashir.setClassName("6th semester");
+
+        // inner class object
         Student.Uniform aashirKaUniform = aashir.new Uniform();
+        Student.Uniform shayanKaUniform = shayan.new Uniform();
+
+        // calling objects
         aashir.setUniform(aashirKaUniform);
         aashir.getUniform(aashirKaUniform);
+        shayan.getUniform(shayanKaUniform);
 
     }
 }
