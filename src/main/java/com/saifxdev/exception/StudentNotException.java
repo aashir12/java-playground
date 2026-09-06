d3ca9a241539c1bf536eda41d159ddd3679337b5
aashir12 <aashirnawaz5@gmail.com>
2026-09-06 23:15:03 +0500

    comment added

--- src/main/java/com/saifxdev/exception/StudentNotException.java
+++ src/main/java/com/saifxdev/exception/StudentNotException.java
@@ -1,6 +1,7 @@
 package com.saifxdev.exception;
 
 public class StudentNotException extends RuntimeException{
+    // students expception handler
     public StudentNotException(String message){
         super(message);
     }
